c1e10391c68a6f68acc85706f5944db413f00f93
YaraA <yaraasy@gmail.com>
2024-08-13 02:23:07 +0100

    add paymentProcessor call to handler and save payment details

--- internal/handlers/payments_handler.go
+++ internal/handlers/payments_handler.go
@@ -2,12 +2,17 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/cko-recruitment/payment-gateway-challenge-go/internal/models"
 	"github.com/cko-recruitment/payment-gateway-challenge-go/internal/repository"
 	"github.com/cko-recruitment/payment-gateway-challenge-go/internal/validator"
-	"github.com/go-chi/chi"
+	paymentProcessor "github.com/cko-recruitment/payment-gateway-challenge-go/third_party/payment_processor"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
 type PaymentsHandler struct {
@@ -42,6 +47,7 @@ func (h *PaymentsHandler) GetHandler() http.HandlerFunc {
 
 func (ph *PaymentsHandler) PostHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// 1. Decode post payment request body
 		var paymentRequest models.PostPaymentRequest
 		if err := json.NewDecoder(r.Body).Decode(&paymentRequest); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
@@ -49,10 +55,32 @@ func (ph *PaymentsHandler) PostHandler() http.HandlerFunc {
 			return
 		}
 
+		// 2. Validate post payment request body
 		if err := validatePaymentRequest(paymentRequest); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte(err.Error()))
 			return
+		}
+
+		// 3. Process payment
+		payment, err := processPayment(paymentRequest)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// 4. Save payment
+		ph.storage.AddPayment(*payment)
+
+		// 5. Convert payment model to payment response json
+		if err := json.NewEncoder(w).Encode(payment); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
 		}
 	}
 }
@@ -60,4 +88,44 @@ func (ph *PaymentsHandler) PostHandler() http.HandlerFunc {
 func validatePaymentRequest(pr models.PostPaymentRequest) error {
 	validator := validator.GetValidator()
 	return validator.ValidateStruct(&pr)
+}
+
+func processPayment(pr models.PostPaymentRequest) (*models.PostPaymentResponse, error) {
+	// 1. Create process payment request
+	processPaymentReq := paymentProcessor.ProcessPaymentRequest{
+		CardNumber: pr.CardNumber,
+		ExpiryDate: time.Date(pr.ExpiryYear, time.Month(pr.ExpiryMonth), 1, 0, 0, 0, 0, time.UTC).Format("01/2006"),
+		Currency:   pr.Currency,
+		Amount:     pr.Amount,
+		Cvv:        pr.Cvv,
+	}
+
+	// 2. Process payment
+	bankPaymentProcessor := paymentProcessor.GetBankPaymentProcessor()
+	processPaymentResp, err := bankPaymentProcessor.ProcessPayment(processPaymentReq)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. Convert process payment request to payment model
+	cardNumberLastFour, err := strconv.Atoi(pr.CardNumber[len(pr.CardNumber)-4:])
+	if err != nil {
+		return nil, fmt.Errorf("error while converting cardNumber to int ")
+	}
+
+	paymentStatus := "Declined"
+	if processPaymentResp.Authorized {
+		paymentStatus = "Authorized"
+	}
+
+	return &models.PostPaymentResponse{
+		Id:                 uuid.New().String(),
+		CardNumberLastFour: cardNumberLastFour,
+		PaymentStatus:      paymentStatus,
+		ExpiryMonth:        pr.ExpiryMonth,
+		ExpiryYear:         pr.ExpiryYear,
+		Currency:           pr.Currency,
+		Amount:             pr.Amount,
+	}, nil
+
 }

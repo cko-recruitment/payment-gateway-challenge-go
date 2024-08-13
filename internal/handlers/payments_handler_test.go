fd73bda735f91049299c98ab9db5b75d3af8fc4b
YaraA <yaraasy@gmail.com>
2024-08-13 02:28:27 +0100

    tests: post payment req body validations

--- internal/handlers/payments_handler_test.go
+++ internal/handlers/payments_handler_test.go
@@ -1,13 +1,16 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/cko-recruitment/payment-gateway-challenge-go/internal/models"
 	"github.com/cko-recruitment/payment-gateway-challenge-go/internal/repository"
-	"github.com/go-chi/chi"
+	"github.com/cko-recruitment/payment-gateway-challenge-go/internal/validator"
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -68,5 +71,390 @@ func TestGetPaymentHandler(t *testing.T) {
 		// Check the HTTP status code in the response
 
 		assert.Equal(t, w.Code, http.StatusNotFound)
+	})
+}
+
+func TestPostPaymentHandler(t *testing.T) {
+	ps := repository.NewPaymentsRepository()
+	payments := NewPaymentsHandler(ps)
+
+	r := chi.NewRouter()
+	r.Post("/api/payments", payments.PostHandler())
+
+	httpServer := &http.Server{
+		Addr:    ":8091",
+		Handler: r,
+	}
+
+	go func() error {
+		return httpServer.ListenAndServe()
+	}()
+
+	validator.NewValidator()
+
+	t.Run("Post Payment will return 400 as CardNumber contains alphabets", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "AA22405343248112",
+			ExpiryMonth: 10,
+			ExpiryYear:  2035,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as CardNumber length is less than 14 digits", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2224053432481",
+			ExpiryMonth: 10,
+			ExpiryYear:  2035,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as CardNumber length is greater than 19 digits", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "22240534324811234567",
+			ExpiryMonth: 10,
+			ExpiryYear:  2035,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as ExpiryMonth is less than 1", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 0,
+			ExpiryYear:  2035,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as ExpiryMonth is greater than 12", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 13,
+			ExpiryYear:  2035,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+
+	t.Run("Post Payment will return 400 as ExpiryYear as number of digits is greater than 4", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  20355,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as ExpiryYear as number of digits is less than 4", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  203,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as ExpiryYear and ExpiryMonth are not in the future", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  2024,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as Currency length is less than 3", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  2040,
+			Currency:    "GB",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as Currency length is greater than 3", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  2040,
+			Currency:    "GBPP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as Currency is not a correct ISO currency", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  2040,
+			Currency:    "EPP",
+			Amount:      100,
+			Cvv:         123,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as CVV number of digits are less than 3", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  2040,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         12,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
+	})
+	t.Run("Post Payment will return 400 as CVV number of digits are greater than 4", func(t *testing.T) {
+		// Create a new HTTP request for testing
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(&models.PostPaymentRequest{
+			CardNumber:  "2222405343248112",
+			ExpiryMonth: 1,
+			ExpiryYear:  2040,
+			Currency:    "GBP",
+			Amount:      100,
+			Cvv:         12345,
+		})
+
+		req, _ := http.NewRequest("POST", "/api/payments", &buf)
+
+		// Create a new HTTP request recorder for recording the response
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		// Check the body is not nil
+		assert.NotNil(t, w.Body)
+
+		// Check the HTTP status code in the response
+		if status := w.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusBadRequest)
+		}
 	})
 }

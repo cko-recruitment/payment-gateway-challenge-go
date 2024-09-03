3941f9d9c94d4dc04f66f61f57ed7eb41f87142f
nick-saxelby-cko <88375324+nick-saxelby-cko@users.noreply.github.com>
2024-09-03 16:27:01 +0100

    feat: Change the base Go challenge to include more boiler plate code (#8)
    * Provide GET endpoint and one failing test
    
    * Change readme link back to main branch

--- docs/docs.go
+++ docs/docs.go
@@ -51,7 +51,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "",
-	Host:             "localhost:8080",
+	Host:             "localhost:8090",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Payment Gateway Challenge Go",
